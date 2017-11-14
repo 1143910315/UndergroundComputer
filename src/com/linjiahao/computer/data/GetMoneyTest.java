e2f802cd3cf1d19ce160d01b99c1c86196b7d5c7
独孤人 <1143910315@qq.com>
2017-11-14 18:10:38 +0800

    获取地铁票价

---
+++ src/com/linjiahao/computer/data/GetMoneyTest.java
@@ -0,0 +1,11 @@
+package com.linjiahao.computer.data;
+
+
+import org.junit.Test;
+
+public class GetMoneyTest {
+    @Test
+    public void sendGet() throws Exception {
+        System.out.println(GetMoney.sendGet("西朗", "坑口"));
+    }
+}
