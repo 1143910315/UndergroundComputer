d548dff6ae423b26a59d102a9c3217af7c607c1d
独孤人 <1143910315@qq.com>
2017-11-16 15:02:25 +0800

    增加价格数据

--- src/com/linjiahao/computer/data/Line.java
+++ src/com/linjiahao/computer/data/Line.java
@@ -1,7 +1,9 @@
 package com.linjiahao.computer.data;
+import java.io.Serializable;
 import java.util.ArrayList;
-public class Line {
+public class Line implements Serializable {
 	private ArrayList<String> lineData = new ArrayList<>();
+	private ArrayList<Integer> price=new ArrayList<>();
 	protected Line add(String station) {
 		lineData.add(station);
 		return this;
